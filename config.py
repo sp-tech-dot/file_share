7860bdc9f947cf1c117a9aa3802bef888a94a4fd
sp-tech-dot <123059116+sp-tech-dot@users.noreply.github.com>
2023-01-21 12:20:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5477555744"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://spfile:12345@cluster0.qcgexiw.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://spfile:sp123@cluster1.dfbfr7t.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "spfile")
 
 #force sub channel id, if you want enable force sub
